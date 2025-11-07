40bf0c15b293c243665f6dbe89fe3cdb9082e791
parsa-mir <parsa.miraghaie@gmail.com>
2025-11-07 10:58:49 +0200

    chore: vercel fixes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
+import { handle } from "hono/vercel";
 import api from "../src/api";
 
 const app = new Hono();
 app.route("/api", api);
 
-export default app.fetch;
+export default handle(app);
